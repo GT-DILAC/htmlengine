18fcbc7424c01af715e477ca81380e16ab747f34
Aditya Anupam <aditya.anupam91@gmail.com>
2020-07-28 09:04:32 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# htmlengine
-An html-based simulation engine for creating COVID-related games based on epidemiological data. 
+SIR Model README
+
+The example.js file is the best place to start as it uses and explains all the key functions available in the SIR model source code.
+
+If you wish to change the model directly, the SIRModel.bundle.js file is the place to look. It should only be modified after line 800 (var SIR_function) onwards.
+
+
+Basic Information about the SIR Model:
+The following will help you learn what the basic SIR Model is.
+
+The SIR Model is an epidemiological model consisting of three dependent parameters:
+- The Susceptible fraction of the population (S) over time, i.e,. those who have not yet contracted the virus
+– The Infected fraction of the population (I) over time, i.e,. those who have the virus
+– The Recovered fraction of the population (R) over time, i.e., those who had the virus, but have healed and cannot get the virus again
+
+These three parameters are calculated over time using the following independent parameters:
+– The number of days that you want to observe the pandemic for
+– Transmission rate of the virus, 
+– Recovery rate (usually 1/days),
+– Initial Susceptible Fraction (S0), 
+– Initial Infected Fraction (I0)
+
+Once you have the S, I, R fractions for each day and shift, then you can use the information to calculate the chances of getting infected (see example.js). These fractions will tell you what the status of the people in a location is generally like. For example, if: 
+S = 0.5 (50%), 
+I = 0.4 (40%), and 
+R = 0.1 (10%) on a given day and place, 
+Then for a total of 10 people in that place, you can expect: 
+Total Susceptible People = S x Total people = 0.5 x 10 = 5
+Total Infected People = I x Total people = 0.4 x 10 = 4
+Total Recovered People = R x Total people = 0.1 x 10 = 1
+
+Once you know the number of infected people in a location, then you can assume probabilities of running into those people (Probability of Contact) and your catching the infection from them (Probability of Catching Infection). Using these probabilities, you can gauge your chance of being infected by: 
+Total Infected People at location = Total People at location x I 
+Probability of Getting Infected = Total Infected People x Probability of Contact x Probability of Catching Infection
+
+So if there were 4 infected people, and the probability of running into them was 0.3, and the probability of catching infection which is usually around 0.05, then:
+Probability of Getting Infected = 4 x 0.3 x 0.05 = 0.06 or 6%
+
+On the other hand, you can also choose to not use the S, I, R calculation and simply calculate the chance of infection if you already know how many infected people are present. For example, if you know there are 6 total infected people at a location, then you may directly use:
+Probability of Getting Infected = Total Infected People (6) x Probability of Contact x Probability of Catching Infection
+
+
+
---
+++ example.js
@@ -0,0 +1,81 @@
+/**the sirModel.CityParams function sets the initial conditions and independent parameters of a city: 
+ * Transmission rate, 
+ * Total Days that you want t, observe the pandemic form
+ * Recovery rate (1/days), 
+ * Initial Susceptible Fraction (S0), 
+ * Initial Infected Fraction (I0)*/
+var transmissionRate = 0.3;
+var totalDays = 14;
+var recoveryRate = 1/totalDays;
+var S0 = 0.933;
+var I0 = 0.0611;
+var NewYorkParams = new sirModel.CityParams(0.3, 1 / 14, 0.933, 0.0661);
+
+/** The sirModel.SIRMODEL function calculates the SIR values 
+ * for the given city 
+ * for the given number of days,
+ * for the given shifts per day 
+It stores them in a totalShifts x 3 array */
+var newYorkSIRData = sirModel.SIRMODEL(NewYorkParams, 21, 2);
+
+
+/** The sirModel.Infection_prob_city function returns the probability of infection for a given location using: 
+    a given city's SIR data (e.g., newYorkSIRData defined above), 
+    the current day since start of game starting with first day as 1 (e.g., 18)
+    the current shift number on that day (e.g., 1 (Morning) or 2 (Afternoon), etc), 
+    total number of people at the location (e.g., 30), 
+    probability of coming into contact with an infected person at that location (e.g., 0.3)
+    probability that contact with an infected person will spread infection (usually 0.05)
+
+    This function uses the first 4 parameters to calculate how many sick people are there in a location.
+    It then multiplies that answer with the probability of contacting them and the probability of infection spreading upon contact with them.
+    */
+var currentDay = 20;
+var currentShift = 1;
+var numPeopleAtLocation = 30;
+var contactProb = 0.3;
+var infectiousContactProb = 0.05;
+var infectionProb = sirModel.Infection_prob_city(newYorkSIRData,currentDay,currentShift,numPeopleAtLocation,contactProb,infectiousContactProb);
+
+
+//The sirModel.IsInfection function takes infection probability as an input and returns true or false depending on whether one gets infected */
+var isInfected = sirModel.IsInfected(infectionProb);
+
+//The sirModel.CurrentSIRData function returns the percentage Susceptible (S), Infected (I), and Recovered (R) fractions of the population
+var currentSIR = sirModel.CurrentSIRData(newYorkSIRData, currentDay, currentShift);
+var currentSusceptible = currentSIR[0];
+var currentInfected = currentSIR[1];
+var currentRecovered = currentSIR[2];
+
+
+/** Display infection probability and if a person gets infected. 
+ *  Refresh the index page in the browser to see different results for whether you got infected*/
+console.log("Fraction of population susceptible in New York on day" + currentDay + " shift " + currentShift + ": " + currentSusceptible);
+console.log("Fraction of population infected in New York on day" + currentDay + " shift " + currentShift + ": " + currentInfected);
+console.log("Fraction of population recovered in New York on day" + currentDay + " shift " + currentShift + ": " + currentRecovered);
+console.log("Probability of being infected in a place with " + numPeopleAtLocation + " people in New York." + infectionProb);
+console.log("Are you infected?: " + isInfected);
+
+document.getElementById("infectedProb").innerHTML = infectionProb;
+document.getElementById("infected").innerHTML = isInfected;
+
+
+/** If you already know how many sick people are in a given location, 
+ * then you can use the sirModel.Infection_prob_local function to calculate the probability of infection at a given location. 
+ * This function only needs three inputs:
+ * the number of sick people in the location (e.g., 30)
+ * probability of coming into contact with any person at that location (e.g., 0.8)
+ * probability that contact with an infected person will spread infection (usually 0.05)
+ */
+var numSickPeople = 40;
+var contactProb2 = 0.3;
+var infectiousContactProb2 = 0.05;
+
+var infectionProb2 = sirModel.Infection_prob_local(numSickPeople, contactProb2, infectiousContactProb2);
+var isInfected2 = sirModel.IsInfected(infectionProb2);
+
+console.log("Probability of being infected in a place with " + numSickPeople + " people in New York: " + infectionProb2);
+console.log("Are you infected?: " + isInfected2);
+
+document.getElementById("infectedProb2").innerHTML = infectionProb2;
+document.getElementById("infected2").innerHTML = isInfected2;
